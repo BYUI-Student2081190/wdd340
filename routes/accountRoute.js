7e4c1bfe3b1fa745c2aac2c8a410565c3b49c447
byui_student1045 <sco22022@byui.edu>
2024-10-09 15:30:04 -0600

    Team Activity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -8,13 +8,23 @@ const regValidate = require("../utilities/account-validation")
 // Route to build account - utilities will automatically handle any errors that creep up
 router.get("/login", utilities.handleErrors(accController.buildLogin));
 // Route to build registration
-router.get("/registration", utilities.handleErrors(accController.buildRegistration));
+router.get("/register", utilities.handleErrors(accController.buildRegistration));
+
 // Process the registration data
 router.post(
     '/register',
     regValidate.registrationRules(),
     regValidate.checkRegData,
     utilities.handleErrors(accController.registerAccount)
+);
+// Process the login attempt
+router.post( 
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    (req, res) => {
+      res.status(200).send('login process')
+    }
 );
 
 module.exports = router;
